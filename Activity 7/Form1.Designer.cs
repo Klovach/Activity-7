f9bbe2912dc195d1f31dd57b6fd6e64e96b04aa8
elisa <elisa@DESKTOP-NOHK2QH>
2022-09-30 13:04:30 -0700

    Add project files.

---
+++ Activity 7/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Activity_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTerms = new System.Windows.Forms.Label();
+            this.txtTerms = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTerms
+            // 
+            this.lblTerms.AutoSize = true;
+            this.lblTerms.Location = new System.Drawing.Point(9, 42);
+            this.lblTerms.Name = "lblTerms";
+            this.lblTerms.Size = new System.Drawing.Size(107, 16);
+            this.lblTerms.TabIndex = 0;
+            this.lblTerms.Text = "Enter # of Terms:";
+            // 
+            // txtTerms
+            // 
+            this.txtTerms.Location = new System.Drawing.Point(122, 39);
+            this.txtTerms.Name = "txtTerms";
+            this.txtTerms.Size = new System.Drawing.Size(173, 22);
+            this.txtTerms.TabIndex = 1;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(9, 156);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(172, 16);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "Approximate Value of PI is...";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 85);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(104, 45);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "CALCULATE";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(336, 220);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(67, 32);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 272);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.txtTerms);
+            this.Controls.Add(this.lblTerms);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Approximate PI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTerms;
+        private System.Windows.Forms.TextBox txtTerms;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Activity 7/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            /* Declare and initialize variables. Our input from the terms textbox is assigned to the variable terms.
+             * Our double denominator will be utilized as the denominator of the given formula. Our double, sum,
+             will calculate the sum of the series of integers. */
+            int terms = Convert.ToInt32(txtTerms.Text);
+            double denominator = 1;
+            double sum = 0;
+
+            /* For as long as the number of terms is greater than i, increment the counter by one. 
+            A for loop is ideal in instances where we would like to repeat a block of code a known number of times.
+            In this case, we want our block of code to repeat until we have no more terms to perform this formula on.*/
+            for (int i = 0; i < terms; i++)
+            {
+                if (i % 2 == 0) 
+                {
+                    sum = sum + 4 / denominator;
+                }
+                else
+                {
+                    sum = sum - 4 / denominator;
+                }
+                denominator = denominator + 2;
+            }
+
+            /* This line of code will change the text assigned to the answer label to the text below. The escape characters,
+            /n/n will ensure that the answer is spaced correctly. */
+            lblAnswer.Text = "Approximate value of PI after " + terms + " Terms" + "\n\n" + "=" + sum.ToString();
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Exit application. 
+            Application.Exit(); 
+        }
+    }
+}
